65be8d25bed08772e40f5e3089dfac0cf21cf376
Alex Kozachenko <alex.iMail@icloud.com>
2017-10-08 13:32:07 -0400

    Changed to swift 4

--- DiagramView/NSLayoutConstraint.swift
+++ DiagramView/NSLayoutConstraint.swift
@@ -12,18 +12,18 @@ extension NSLayoutConstraint {
         //height
         let heightContraints = NSLayoutConstraint(item: forView, attribute:
             .height, relatedBy: .equal, toItem: inView,
-                     attribute: NSLayoutAttribute.height, multiplier: 1,
+                     attribute: NSLayoutConstraint.Attribute.height, multiplier: 1,
                      constant: 0)
         
         //width
         let widthContraints = NSLayoutConstraint(item: forView, attribute:
             .width, relatedBy: .equal, toItem: inView,
-                    attribute: NSLayoutAttribute.width, multiplier: 1.0,
+                    attribute: NSLayoutConstraint.Attribute.width, multiplier: 1.0,
                     constant: 0)
         
         //centerY - center Vertically
         
-        let centerY = NSLayoutConstraint(item: forView, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: inView, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)
+        let centerY = NSLayoutConstraint(item: forView, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: inView, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 0)
         
         //centerX - center Horizontally
         let centerX = NSLayoutConstraint(item: forView, attribute: .centerX, relatedBy: .equal, toItem: inView, attribute: .centerX, multiplier: 1.0, constant: 0)
